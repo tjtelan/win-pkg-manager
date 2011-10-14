5ff50951775dfdf44c28b95be697030623aa511e
T.J. Telan <telant@oak.ROCK>
2011-10-14 12:38:02 -0700

    shell ready for testing

--- shell.py
+++ shell.py
@@ -1,58 +1,42 @@
 #!/usr/bin/env python
 
-# installation protocol -- draft
+import os, argparse, logging
 
-# Other program will be responsible for updating the snapshot of the currently installed programs in the database
-# This program will only update the snapshots for the current program if it has not yet been done in the current calendar day
+class shell:
+    # __init__
+    # 
+    # Initializes the command line interface for Windows Package Manager
+    def __init__(self, logFileName):
+        self.parser = argparse.ArgumentParser(description='FreeBSD ports inspired package manager for Windows', prefix_chars='-')
 
-# (-r flag applies first)
-#
-# download program
-# check if dependencies satisfied (if not, recurse)
-# (-B flag)
-# install
-# (-A flag)
-#
-# (apply -R flag now)
-#
-#
-# suggestion: create a port-tree-like hierarchy for this program
+        # Simplified usage message
+        self.parser = argparse.ArgumentParser(prog='winpkg', usage='%(prog)s [options] command ... [pkg_name]', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-# Get help to look similar to this:
-# usage: portsnap [options] command ... [path]
-# where options are flags
-# and commands are update,install,info,remove...
+        # Positional arguments
+        self.parser.add_argument('command', choices=['info', 'update', 'install', 'remove'], help='')
+        self.parser.add_argument('pkg_name', nargs='*', help='Package(s) to act on')
 
-import argparse
+        # Optional flags
+        self.parser.add_argument('-a', '--all', action='store_true', help='Do with all the installed packages')
+        self.parser.add_argument('-A', '--afterinstall', metavar='CMD', nargs=1, help='Run the specified command after each installation')
+        self.parser.add_argument('-B', '--beforeinstall', metavar='CMD', nargs=1, help='Run the specified command before each installation')
+        self.parser.add_argument('-e', '--emit-summaries', action='store_true', help='Emit summary info after each package processing')
+        self.parser.add_argument('-f', '--force', action='store_true', help='Force the upgrade of a package even if it is to be a downgrade or just a reinstall')
+        self.parser.add_argument('-F', '--fetch-only', action='store_true', help='Only fetch packages. Do not build or install anything')
+        self.parser.add_argument('-k', '--keep-going', action='store_true', help='Force the upgrade of a port even if some of the requisite packages have failed to upgrade')
+        self.parser.add_argument('-l', '--log-file', metavar='PATH', nargs=1, type=str, default=os.curdir, help='Use log file at specified location')
+        self.parser.add_argument('-n', '--no-execute', action='store_true', help='Do not upgrade any packages, but just show what would be done')
+        self.parser.add_argument('-O', '--omit-check', action='store_true', help='Omit sanity checks for dependencies')
+        self.parser.add_argument('-r', '--recursive', action='store_true', help='Do with all those depending on the given packages as well')
+        self.parser.add_argument('-R', '--upward-recursive', action='store_true', help='Do with all those required by the given packages as well / Fetch recursively if -F is specified')
+        self.parser.add_argument('-v', '--verbose', action='store_true', help='Be verbose')
+        self.parser.add_argument('-x', '--exclude', metavar='GLOB', nargs='+', help='Exclude packages matching the specified glob pattern')
 
+        #self.args = parser.parse_args()
 
-#class WindowsPackageManager:
-#    def cli(self, argv):
-parser = argparse.ArgumentParser(description='FreeBSD ports inspired package manager for Windows', prefix_chars='-')
-
-
-#parser = argparse.ArgumentParser(prog='PROG', usage='%(prog)s [options] command ... [pkg_name]')
-parser.add_argument('-a', '--all', action='store_true', help='Do with all the installed packages')
-parser.add_argument('-A', '--afterinstall', metavar='CMD', nargs=1, help='Run the specified command after each installation')
-parser.add_argument('-B', '--beforeinstall', metavar='CMD', nargs=1, help='Run the specified command before each installation')
-parser.add_argument('-f', '--force', action='store_true', help='Force the upgrade of a port even if it is to be a downgrade or just a reinstall')
-parser.add_argument('-F', '--fetch-only', action='store_true', help='Only fetch distfiles or packages. Do not build or install anything')
-parser.add_argument('-k', '--keep-going', action='store_true', help='Force the upgrade of a port even if some of the requisite ports have failed to upgrade')
-parser.add_argument('-l', '--log-file', metavar='path', nargs=1, help='Use log file at specified location')
-parser.add_argument('-n', '--no-execute', action='store_true', help='Do not upgrade any ports, but just show what would be done')
-parser.add_argument('-r', '--recursive', action='store_true', help='Do with all those depending on the given packages as well')
-parser.add_argument('-R', '--upward-recursive', action='store_true', help='Do with all those required by the given packages as well / Fetch recursively if -F is specified')
-parser.add_argument('-v', '--verbose', action='store_true', help='Be verbose')
-
-
-parser.add_argument('command', choices=['info', 'update', 'install', 'remove'], help='')
-parser.add_argument('pkg_name', nargs='*', help='')
-
-args = parser.parse_args()
-
-print args
-
-#    def check_version(self, prog):
-#        # query database for program details
-#        # perform method for checking for updates (cli or website or...)
-#        return
+        def cmd(self, argv=None):
+            if len(argv) > 0:
+                self.args = self.parser.parse_args(argv)
+            else:
+                print('DEBUG: empty argv')
+        return
