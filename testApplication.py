f817813b81a9acaacc4b3f83bebe90f460f0b5ad
Sebastian J. Imlay <imlays@CF498-xx.cs.wwu.edu>
2011-11-27 18:59:42 -0800

    Added majority of logic for check updates and dl updates.  Need to know where save the file to.

--- testApplication.py
+++ testApplication.py
@@ -6,27 +6,23 @@ mydb = wpm_db.db("dbFile", "dbLog")
 
 # Add vim to the db.
 vim_name = u'vim'
-vim_url = "www.vim.org"
+vim_url = "http://www.vim.org"
 vim_dl_url = "http://www.vim.org/download.php"
 vim_version = "1.2.3"
 vim_regex = 'href=".*vim.*\.exe"'
 
-if not vim_name in db_wrapper.get_applications(mydb)[1]:
-    #print db_wrapper.get_applications(mydb)
+appLogFileName = "appLog"
+
+validQuery, applications = db_wrapper.get_applications(mydb)
+if validQuery and not vim_name in applications:
     db_wrapper.add_app(mydb, vim_name, vim_version, vim_url, vim_dl_url)
     db_wrapper.add_regex(mydb, vim_name, [vim_regex])
-else:
-    print db_wrapper.get_app_regex(mydb, vim_name)
 
-vim = app("vim", mydb)
+vim = app("vim", mydb, appLogFileName)
 
-vim.checkUpdates()
+#vim.checkUpdates()
 vim.dlUpdates()
 
-
-#vim = Application("vim", "http://www.vim.org/download.php", 'href=".*\.exe"')
-#vim = app("vim", "http://www.vim.org/download.php", 'href=".*vim.*\.exe"')
-#vim.checkUpdates()
 
 #vBox = Application("virtual box", "https://www.virtualbox.org/wiki/Downloads", 'href=".*Win.*\.exe"')
 #
