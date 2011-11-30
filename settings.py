581a89787094b63e45e03d621e394171c83ff6de
T.J. Telan <t.telan@gmail.com>
2011-11-29 21:21:10 -0800

    adding Winpkg configuration file for default paths

---
+++ settings.py
@@ -0,0 +1,12 @@
+import os
+import wpm_db
+import db_wrapper
+#import time
+
+class settings:
+
+    def __init__:
+        DEFAULT = {
+            'PACKAGE_DIR'   : os.curdir + "/packages",
+            'DOWNLOAD_DIR'  : os.curdir + "/downloads"
+        }
