0c12f2478e4b6426877fb1744304d9b38433f574
Sebastian Imlay <sebastian.imlay@gmail.com>
2011-12-02 11:13:01 -0800

    Added more applications to db.

--- testApplication.py
+++ testApplication.py
@@ -5,7 +5,7 @@ from wpm_app import app
 mydb = wpm_db.db("dbFile", "dbLog")
 
 # Add vim to the db.
-vim_name = unicode('vim')
+vim_name = unicode('gvim')
 vim_url = "http://www.vim.org"
 vim_dl_url = "http://www.vim.org/download.php"
 vim_version = "1.2.3"
@@ -17,22 +17,83 @@ appLogFileName = "appLog"
 
 validQuery, applications = db_wrapper.get_applications(mydb)
 
-print applications
+print 'All Applicaitons:', applications
 if validQuery and not vim_name in applications:
+    print 'adding vim to the db.'
     db_wrapper.add_app(mydb, vim_name, vim_version, vim_dl_url, vim_url)
     db_wrapper.add_exe_regex(mydb, vim_name, [vim_regex])
     db_wrapper.add_version_regex(mydb, vim_name, [vim_version_regex])
 
-vim = app("vim", mydb, appLogFileName)
-print vim.getExeURLs()
+vim = app(vim_name, mydb, appLogFileName)
+#print 'gvim urls:', vim.getExeURLs()
 
-vim.checkUpdates()
-vim.dlUpdates()
+updatesAvail = vim.checkUpdates()
+print 'gvim update available?', updatesAvail
 
-foo = app("vim", mydb, appLogFileName)
-foo.dlUpdates()
-foo.checkUpdates()
+if updatesAvail:
+    vim.dlUpdates()
 
+putty_name = 'putty'
+putty_url = 'http://www.chiark.greenend.org.uk/~sgtatham/putty/download.html'
+putt_dl_url = 'http://www.chiark.greenend.org.uk/~sgtatham/putty/download.html'
+putty_version = '1.0'
+putty_regex = '".*putty.*\.exe"'
+putty_version_regex = '\d+.*\d'
+
+if validQuery and putty_name not in applications:
+    print 'adding putty to the db.'
+    db_wrapper.add_app(mydb, putty_name, putty_version, putt_dl_url, putty_url)
+    db_wrapper.add_exe_regex(mydb, putty_name, [putty_regex])
+    db_wrapper.add_version_regex(mydb, putty_name, [putty_version_regex])
+
+putty = app(putty_name, mydb, appLogFileName)
+
+#print 'putty urls:', putty.getExeURLs()
+updatesAvail = putty.checkUpdates()
+print 'putty updates available? ', updatesAvail
+if updatesAvail:
+    putty.dlUpdates()
+
+
+winscp_name = 'winscp'
+winscp_url = 'http://winscp.net/eng/download.php'
+winscp_version = '1.0'
+winscp_regex = '".*winscp.*\.exe"'
+winscp_version_regex = '\d+.*\d'
+if validQuery and winscp_name not in applications:
+    print 'adding winscp to the db.'
+    db_wrapper.add_app(mydb, winscp_name, winscp_version, winscp_url, winscp_url)
+    db_wrapper.add_exe_regex(mydb, winscp_name, [winscp_regex])
+    #db_wrapper.add_version_regex(mydb, winscp_name, [winscp_version_regex])
+
+winscp = app(winscp_name, mydb, appLogFileName)
+updatesAvail = winscp.checkUpdates()
+print 'winscp updates available? ', updatesAvail
+if updatesAvail:
+    winscp.dlUpdates()
+
+
+SevenZip_name = '7zip'
+SevenZip_url = 'http://www.7-zip.org/download.html'
+SevenZip_version = '1.0'
+SevenZip_regex = '"http.*7z.*\.exe"'
+SevenZip_version_regex = '\d+'
+if validQuery and SevenZip_name not in applications:
+    print 'adding SevenZip to the db.'
+    db_wrapper.add_app(mydb, SevenZip_name, SevenZip_version, SevenZip_url, SevenZip_url)
+    db_wrapper.add_exe_regex(mydb, SevenZip_name, [SevenZip_regex])
+    db_wrapper.add_version_regex(mydb, SevenZip_name, [SevenZip_version_regex])
+
+SevenZip = app(SevenZip_name, mydb, appLogFileName)
+
+updatesAvail = SevenZip.checkUpdates()
+print '7zip update available? ', updatesAvail
+if updatesAvail:
+    SevenZip.dlUpdates()
+
+
+
+#
 #vBox = Application("virtual box", "https://www.virtualbox.org/wiki/Downloads", 'href=".*Win.*\.exe"')
 #
 #vBox.checkUpdates()
