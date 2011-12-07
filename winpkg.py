e3a1833ae5460c2514c211aefb7f3b50d828a35b
T.J. Telan <t.telan@gmail.com>
2011-12-07 07:18:16 -0800

    Adding a little note about settings.py to shell wrapper

--- winpkg.py
+++ winpkg.py
@@ -7,6 +7,8 @@ sys.path.append(os.curdir)
 import sqlite3, logging
 from wpm_shell import shell
 
+# TODO: Load settings.py and pass relevant settings to shell
+
 x = shell('logfile')
 
 x.cmd(sys.argv)
